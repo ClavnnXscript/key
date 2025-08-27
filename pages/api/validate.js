51481ff86508c9d672b315521a1b07d0b9d99aea
ClavnnXscript <ckpai0576@gmail.com>
2025-08-27 15:45:24 +0700

    Update validate.js

--- pages/api/validate.js
+++ pages/api/validate.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 )
 
 export default async function handler(req, res) {
@@ -20,7 +20,6 @@ export default async function handler(req, res) {
   }
 
   try {
-    // ✅ kolom di DB namanya license_key (bukan key)
     const { data, error } = await supabase
       .from('keys')
       .select('*')
@@ -51,4 +50,4 @@ export default async function handler(req, res) {
       message: 'Internal server error' 
     })
   }
-        }
+}
